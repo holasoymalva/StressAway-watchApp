a0d58ef7deef9da7df98d1e7e37b98b5a65ec1a8
holasoymalva <alquimista3301@gmail.com>
2025-01-13 23:47:51 -0600

    Initial Commit

---
+++ StressAway Watch App/StressAwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  StressAwayApp.swift
+//  StressAway Watch App
+//
+//  Created by malva on 13/01/25.
+//
+
+import SwiftUI
+
+@main
+struct StressAway_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StressAway Watch AppTests/StressAway_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StressAway_Watch_AppTests.swift
+//  StressAway Watch AppTests
+//
+//  Created by malva on 13/01/25.
+//
+
+import Testing
+@testable import StressAway_Watch_App
+
+struct StressAway_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
